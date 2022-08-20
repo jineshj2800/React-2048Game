136252cc786d497deacc75450ba0df13db09f72b
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2022-08-20 17:25:45 +0530

    feat: Adds comments for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,25 @@ import React from "react";
 import "../styles.css";
 
 export default function Header({ score }) {
+  /*
+    You should not read from local storage on every render because
+    reading from local storage is expensive (i.e. is time consuming).
+    In fact, it should be read only once.
+
+    Also, you should not write to local storage in the render cycle
+    as it is an effect-ful thing and hence must be done in useEffect.
+
+    So, you could rewrite this bit of code like this:
+
+    const [bestStore, setBestScore] = useState(() => window.localStorage.getItem("bestScore") || 0);
+
+    useEffect(() => {
+      if(bestScore < score) {
+        setBestScore(score);
+        window.localStorage.setItem("bestScore", score);
+      }
+    }, [score]);
+  */
   let bestScore = window.localStorage.getItem("bestScore") || 0;
   if (bestScore < score) {
     bestScore = score;
