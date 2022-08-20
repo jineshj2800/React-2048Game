6f8bad0589ebae10c88a2929ed12ec883832b72c
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2022-08-20 16:49:06 +0530

    feat: Added comments for Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,11 +13,48 @@ function areEqual(arg1, arg2) {
 export default function Board({ score, onScoreChange }) {
   const [gameState, setGameState] = useState(initialState);
 
+  /*
+    We must not think of useEffect dependency array as something
+    which should be used to re-run the effect callback. Instead,
+    we must put all the dependencies (i.e. all variables used in
+    the callback) inside the dependency array and then put the
+    condition inside the callback itself.
+    Like this:
+
+    useEffect(() => {
+      if(areEqual(gameState, initialState)) {
+        const newGameState = generatetNewRandomCell(gameState)
+        setGameState(newGameState)
+      }
+    }, [gameState, initialState])
+
+    You do not need to optimize the number of times the effect callback
+    function is called. You need to add all the variables used inside
+    the effect callback to the dependency array, there are very few
+    exceptions to this rule.
+  */
   useEffect(() => {
     const newGameState = generatetNewRandomCell(gameState);
     setGameState(newGameState);
   }, [areEqual(gameState, initialState)]);
 
+  /*
+    Again, here also in the dependency array, updateBoard should have
+    been added because it is a local variable and is being used inside
+    the effect callback.
+    But, also note that updateBoard is being created afresh in every
+    render, so if you add it to the dependency array, then the effect
+    callback will run on each render. So, you must wrap updateBoard 
+    in useCallback so that it is "not created" on every render.
+
+    const updateBoard = useCallback((gameState, keyPressed) => {
+      ...
+    }, [onScoreChange]);
+
+    Also, always try to use variables only after declaration, then the flow
+    of the code is easy to understand. For example, put the code of updateBoard
+    before its usage in the effect callback.
+  */
   useEffect(() => {
     function handleKeyDown(e) {
       const shiftedGameState = updateBoard(gameState, e.key);
@@ -68,6 +105,14 @@ export default function Board({ score, onScoreChange }) {
     return cellValues;
   }
 
+  /*
+    This function can be moved outside the render of Board component
+    as it does not use any information from the closure (i.e. state or
+    local variables). 
+    By moving it outside, you will not be recreating it on each render.
+    Also, then you would not have to add it to the dependency array of
+    effect callbacks.
+  */
   function generatetNewRandomCell(gameState) {
     const allCells = [...gameState];
     const emptyCells = [];
@@ -84,6 +129,15 @@ export default function Board({ score, onScoreChange }) {
     return allCells;
   }
 
+  /*
+    Wrap this function in useCallback as we don't know if it could
+    be used in some dependency array in child component.
+
+    const handleRestart = useCallback(() => {
+      onScoreChange(0);
+      setGameState(initialState);
+    }, [onScoreChange]);
+  */
   function handleRestart() {
     onScoreChange(0);
     setGameState(initialState);
