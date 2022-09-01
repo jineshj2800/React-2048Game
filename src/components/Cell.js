74380850d814751c808656ece82e70df99bc5c56
Jinesh Jain <jineshjain@Jineshs-MacBook-Pro-2.local>
2022-09-01 16:57:38 +0530

    Delete: remove comments

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,20 +1,5 @@
 import React from "react";
 
-/* 
-  We prefer arrow functions over regular functions as they
-  are compact and concise. For example, we can rewrite Cell
-  as an arrow function as follows:
-
-  const Cell = ({ value }) => (
-    <div className={"cell" + (value ? ` cell-${value}` : "")}>
-      {value ? value : null}
-    </div>
-  )
-
-  But then you need to export it as default this way:
-
-  export default Cell;
-*/
 export default function Cell({ value }) {
   return (
     <div className={"cell" + (value ? ` cell-${value}` : "")}>
